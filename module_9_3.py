0a985b89644940cab6238729460e1c0b3f2cc340
Roman Shmatko <rnshmatko@gmail.com>
2024-08-29 02:27:44 +0300

    Откорректированное практические задания по урокам: генераторные сборки (module_9_3.py).

--- module_9_3.py
+++ module_9_3.py
@@ -3,7 +3,7 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 # Формирование переменных срезультатами
-first_result = list(filter(lambda y: y != 0, map(lambda x: len(x[0]) - len(x[1]), zip(first, second))))
+first_result = list(filter(lambda z: z != 0, map(lambda x, y: len(x) - len(y), first, second)))
 second_result = list(map(lambda i: len(first[i]) == len(second[i]), range(len(first))))
 
 # Вывод результатов
