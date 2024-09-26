3df77449e6d4a7203bfe0e469b66cbf466b8174b
Roman Shmatko <rnshmatko@gmail.com>
2024-09-26 13:31:58 +0300

    Практическое задание по урокам: методы Юнит-Тестирования (teste_12_2.py) откорректированное

--- runner_and_tournament.py
+++ runner_and_tournament.py
@@ -27,15 +27,23 @@ class Tournament:
         self.participants = sorted(list(participants), key=lambda x: x.speed, reverse=True)
 
     def start(self):
-        finishers = {}
-        place = 1
-        while self.participants:
-            for participant in self.participants:
-                participant.run()
-                if participant.distance >= self.full_distance:
-                    # Изменено формирование значений словаря
-                    finishers[place] = str(participant)
-                    place += 1
-                    self.participants.remove(participant)
+        # ------- 1-й вариант -------
+        # finishers = {}
+        # place = 1
+        # while self.participants:
+        #     for participant in self.participants:
+        #         participant.run()
+        #         if participant.distance >= self.full_distance:
+        #             # Изменено формирование значений словаря
+        #             finishers[place] = str(participant)
+        #             place += 1
+        #             self.participants.remove(participant)
+        # ----------------------------
+
+        # ------- 2-й вариант -------
+        finishers = dict(map(lambda x: (x[0] + 1, str(x[1])), enumerate(self.participants)))
+        self.participants.clear()
+        # ----------------------------
+
 
         return finishers
