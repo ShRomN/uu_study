acd29c1125c031b1f1c10385c07535b3ef97932b
Roman Shmatko <rnshmatko@gmail.com>
2024-10-07 18:47:04 +0300

    Практическое задание по урокам: написание примитивной ORM (module_14_4.py)

--- crud_functions.py
+++ crud_functions.py
@@ -11,7 +11,7 @@ def initiate_db():
     connection = sqlite3.connect(db_name)
     cursor = connection.cursor()
 
-    # Создаем таблицу в БД с требуемыми полями
+    # Создаем таблицу Products в БД с требуемыми полями
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS Products(
         id INTEGER PRIMARY KEY,
@@ -21,14 +21,25 @@ def initiate_db():
         '''
     )
 
-    # # Заполняем базу данных 4-мя значениями
+    # # Заполняем таблицу Products базы данных 4-мя значениями
     # for i in range(1, 5):
     #     cursor.execute(
     #         'INSERT INTO Products(title, description, price) VALUES (?, ?, ?)',
     #         (f'Продукт {i}', f'Описание {i}', i * 100)
     #     )
 
-    # Применяем изменения
+    # Создаем таблицу Users в БД с требуемыми полями
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL);
+        '''
+    )
+
+    # Применяем изменения БД
     connection.commit()
     # Завершаем соединение
     connection.close()
@@ -36,7 +47,7 @@ def initiate_db():
 
 def get_all_products():
     """
-    AФункция получения всех записей базы данных.
+    Функция получения всех записей базы данных.
     """
     # Подключаемся к БД
     db_name = 'products_db.db'
@@ -50,3 +61,55 @@ def get_all_products():
     connection.close()
 
     return products
+
+
+def is_included(username:str):
+    """
+    Функция проверки наличия пользователя в таблице Users БД.
+    :param username: имя пользователя.
+    :return: True если пользователь есть, иначе False.
+    """
+    # Подключаемся к БД
+    db_name = 'products_db.db'
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+
+    # Проверяем наличие пользователя в таблице
+    cursor.execute(
+        'SELECT * FROM Users WHERE username = ?;',
+        (username, )
+    )
+
+    user = cursor.fetchone()
+
+    # Завершаем соединение
+    connection.close()
+    # print(user)
+    return bool(user)
+
+
+def add_user(username:str, email:str, age:int, balance:int):
+    """
+    Функция добавления нового пользователя в таблицу Users.
+    :param username: имя пользователя;
+    :param email: адрес электронной почты;
+    :param age: возраст пользователя;
+    :param balance: баланс пользователя.
+    """
+    # Подключаемся к БД
+    db_name = 'products_db.db'
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+
+    # Если пользователя нет в БД
+    if not is_included(username):
+        # Добавляем нового пользователя в таблицу
+        cursor.execute(
+            'INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?)',
+            (username, email, age, balance)
+        )
+
+    # Применяем изменения БД
+    connection.commit()
+    # Завершаем соединение
+    connection.close()
