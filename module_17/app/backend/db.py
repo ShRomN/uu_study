940f58d6837641b5acfa40b38ef10ae44ac3a46d
Roman Shmatko <rnshmatko@gmail.com>
2024-11-21 18:02:10 +0300

    Практическое задание по урокам: Модели SQLAlchemy. Отношения между таблицами (module_17_2)

---
+++ module_17/app/backend/db.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+# Создаем движек
+engin = create_engine('sqlite:///taskmanager.db', echo=True)
+
+# Создаем сессию
+SessionLockal = sessionmaker(bind=engin)
+
+class Base(DeclarativeBase):
+    pass
---
+++ module_17/app/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.backend.db import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True) # целое число, первичный ключ, с индексом
+    username = Column(String) # строка
+    firstname = Column(String) # строка
+    lastname = Column(String) # строка
+    age = Column(Integer) # целое число
+    slug = Column(String, unique=True, index=True) # строка, уникальная, с индексом
+    tasks = relationship('Task', back_populates='user') # объект связи с таблицей с таблицей Task, где back_populates='user'
+
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(User.__table__))
