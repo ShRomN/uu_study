f50c841b9d92747698df3fb395829508443f53df
Roman Shmatko <rnshmatko@gmail.com>
2024-12-11 23:11:14 +0300

    Практическое задание по урокам: Модели баз данных в Django (module_19_1)

---
+++ module_19/UrbanDjango2/task1/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Buyer(models.Model):
+    """
+    Класс модели содержащий информацию о покупателе.
+    """
+    name = models.CharField(verbose_name='Имя покупателя', max_length=50)
+    balance = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Баланс счета покупателя')
+    age = models.IntegerField(verbose_name='Возраст покупателя')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Game(models.Model):
+    """
+    Класс модели содержащий информацию о игре.
+    """
+    title = models.CharField(verbose_name='Название игры', max_length=100)
+    cost = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Цена игры')
+    size = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Размер файлов игры')
+    description = models.TextField(verbose_name='Описание игры')
+    age_limited = models.BooleanField(verbose_name='Ограничение возраста для игры', default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='byers')
+
+    def __str__(self) -> str:
+        return self.title
